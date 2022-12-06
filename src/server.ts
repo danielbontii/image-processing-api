920d2531b26f0708ec774b4f0848bd66016858ba
danielbontii <bontiidaniel97@gmail.com>
2022-12-06 17:51:53 +0000

    added endpoint test

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
--- src/tests/conversionSpec.ts
+++ src/tests/conversionSpec.ts
@@ -1,1 +1,19 @@
 //TODO: implement conversion tests
+
+import supertest from 'supertest';
+import app from '../server';
+
+const req = supertest(app);
+
+describe('Conversion enpoint provides the appropriate respone', () => {
+  it('should return status code 200 after conversion is complete', async () => {
+    const result = await req.get(
+      '/img-pro-api/api/v1/convert/?filename=monkey&height=300&width=200'
+    );
+    expect(result.statusCode).toBe(200);
+  });
+  it('should serve original image with status code 200 if height and width are not provided', async () => {
+    const result = await req.get('/img-pro-api/api/v1/convert/?filename=monkey');
+    expect(result.statusCode).toBe(200);
+  });
+});
