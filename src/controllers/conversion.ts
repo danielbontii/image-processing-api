2b723fc1ce5a49e6843c62087279093895584f2f
danielbontii <bontiidaniel97@gmail.com>
2022-12-17 17:53:03 +0000

    added error handling

--- src/controllers/conversion.ts
+++ src/controllers/conversion.ts
@@ -7,29 +7,33 @@ const convert = async (
   req: Request,
   res: Response
 ): Promise<Response | void> => {
-  const filename = req.filename ?? 'default';
-  const conversionWidth = req.conversionWidth;
-  const conversionHeight = req.conversionHeight;
+  try {
+    const filename = req.filename;
+    const conversionWidth = req.conversionWidth;
+    const conversionHeight = req.conversionHeight;
 
-  const inputPath: string = path.join(
-    __dirname,
-    `../images/${filename}.${req.input}`
-  );
+    const inputPath: string = path.join(
+      __dirname,
+      `../images/${filename}.${req.input}`
+    );
 
-  const outputPath = path.join(
-    __dirname,
-    `../images/thumb/${req.filename}_${conversionWidth ?? 'auto'}x${
-      conversionHeight ?? 'auto'
-    }.${req.output}`
-  );
+    const outputPath = path.join(
+      __dirname,
+      `../images/thumb/${req.filename}_${conversionWidth ?? 'auto'}x${
+        conversionHeight ?? 'auto'
+      }.${req.output}`
+    );
 
-  await sharp(inputPath)
-    .resize(conversionWidth, conversionHeight, {
-      kernel: sharp.kernel.nearest,
-      fit: 'cover'
-    })
-    .toFile(outputPath);
-  res.status(StatusCodes.OK).sendFile(outputPath);
+    await sharp(inputPath)
+      .resize(conversionWidth, conversionHeight, {
+        kernel: sharp.kernel.nearest,
+        fit: 'cover'
+      })
+      .toFile(outputPath);
+    res.status(StatusCodes.OK).sendFile(outputPath);
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).render('pages/error');
+  }
 };
 
 export { convert };
